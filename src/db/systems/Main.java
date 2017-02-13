1ef28fe2bb964f4b62e7121081f9262d8ec13520
Devin Crane <devin.crane@gmail.com>
2017-02-13 16:58:58 -0600

    with Table class and data structures

--- src/db/systems/Main.java
+++ src/db/systems/Main.java
@@ -25,22 +25,21 @@ public class Main {
 		
 		try {
             Class.forName("com.vertica.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             // Could not find the driver class. Likely an issue
             // with finding the .jar file.
             System.err.println("Could not find the JDBC driver class.");
             e.printStackTrace();
-            return;
-		}
+            return; 
+        }
 
         // Create property object to hold username & password
         Properties myProp = new Properties();
         myProp.put("user", "team06");
         myProp.put("password", "5ZvwXNCE");
-        
-		Connection connection = null;
-		try {
-			connection = DriverManager.getConnection(
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(
 					"jdbc:vertica://129.7.243.243:5433/cosc6340s17", myProp);
 		} catch (SQLException e) {
             System.err.println("Could not connect to database.");
@@ -52,20 +51,69 @@ public class Main {
 			System.out.println("Connection successful.");
 		} else {
 			System.out.println("Failed to make connection!");
+			return;
 		}
 
+		extractColumns();
+		extractData(connection);
+    	
+    	verify1NF(connection);
+	}
+	
+	public static ResultSet executeQuery(String sqlStatement) {
 		try {
+			return st.executeQuery(sqlStatement);
 		} catch (SQLException e) {
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
+	private static void verify1NF(Connection connection)
+	{
 		try {
+			// TODO: use HashMap to iterate through table schemas
+			for (Table t : tables) {
+				st = connection.createStatement();
+				String query = "select count(*) from public." + t.name;
+				int index = 0;
+				String whereClause = "";
+				for (String key : t.keys) {
+					whereClause += (index == 0 ? "" : " or ") + key + " IS NULL";
+					index++;
+				}
+				if (!whereClause.isEmpty()) {
+					query += " where " + whereClause;
+				}
+				
+	//			String query = "select * from public.testt";
+				ResultSet rs = executeQuery(query);
+	            while(rs.next()){
+	            	if (rs.getInt(1) > 0) {
+	            		System.out.println("1NF Validation Error: null candidate key");
+	            		return;
+	            	}
+	            }
+	            
+	            query = "select count(*) employeeid from public." + t.name + " group by employeeid";
+	            rs = executeQuery(query);
+	            while(rs.next()){
+	            	if (rs.getInt(1) > 1) {
+	            		System.out.println("1NF Validation Error: duplicate candidate keys");
+	            		return;
+	            	}
+	            }
+			}
 		} catch (SQLException e) {
+			System.out.println("1NF query failed.");
+			System.out.println(e.getMessage());
 			e.printStackTrace();
+			
+			return;
 		}
 	}
 	
+	private static void extractColumns()
 	{
 		BufferedReader br = null;
 		try {
@@ -78,7 +126,12 @@ public class Main {
 	        	String cols = schema.substring(schema.indexOf("(")+1, schema.length()-1);
 	        	
 	        	String[] columns = cols.split(",");
+	        	Table table = new Table(tableName, columns);
+	        	tables.add(table);
+	        	tableData.put(tableName, table);
 	        	
+//	        	candidateKeysStr = candidateKeysStr.substring(0, candidateKeysStr.length()-1);
+//	        	tableSchemas.put(tableName, candidateKeysStr);
 	        }
 		} catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -93,6 +146,40 @@ public class Main {
                 
             }
         }
+	}
+	
+	private static void extractData(Connection connection)
+	{
+		try {
+			for (Table t : tables) {
+				ArrayList<String> cols = new ArrayList<>();
+				ArrayList<Integer> colData = new ArrayList<>();
+				cols.addAll(t.keys);
+				cols.addAll(t.nonKeys);
+				// TODO: use HashMap to iterate through table schemas
+				for (String col : cols) {
+					st = connection.createStatement();
+					String query = "select " + col + " from public." + t.name;
+					
+					ResultSet rs = executeQuery(query);
+		            while(rs.next()){
+		            	colData.add(rs.getInt(1));
+		            }
+					int[] colDataTransfer = new int[colData.size()];
+					for (int i = 0; i < colData.size(); i++) {
+						colDataTransfer[i] = colData.get(i);
+					}
+					
+					t.colData.put(col, colDataTransfer);
+		        }
+			}
+		} catch (SQLException e) {
+			System.out.println("1NF query failed.");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			
+			return;
+		}
 	}
 
 }
